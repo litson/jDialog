1f036778a9ced5f6992ca84801092bdf50bb0d3f
张涛 <litson.zhang@gmail.com>
2015-01-06 20:22:21 +0800

    jDialog.toast computed height bug Fixed;

--- dist/jDialog.js
+++ dist/jDialog.js
@@ -843,11 +843,12 @@
      *  封装一些常用的dialog
      */
     jDialog.extend({
-        alert: function(message) {
+        alert: function (message) {
             return jDialog(message);
         },
-        toast: function(message, delay) {
-            var dialog = jDialog(message);
+        toast: function (message, delay) {
+            var dialog = jDialog(message)
+                .addClass('dialog-toast');
             var container = dialog.getContainer();
             var height = dialog.height(container);
             dialog.getContainer().style.textAlign = 'center';
@@ -856,11 +857,10 @@
                 .hideHeader()
                 .hideModal()
                 .height(height)
-                .addClass('dialog-toast')
                 .autoHide(delay || 3);
             return dialog;
         },
-        error: function(message, callBack) {
+        error: function (message, callBack) {
             return jDialog(message, callBack).addClass('dialog-error');
         }
     });
--- src/components.js
+++ src/components.js
@@ -2,11 +2,12 @@
  *  封装一些常用的dialog
  */
 jDialog.extend({
-    alert: function(message) {
+    alert: function (message) {
         return jDialog(message);
     },
-    toast: function(message, delay) {
-        var dialog = jDialog(message);
+    toast: function (message, delay) {
+        var dialog = jDialog(message)
+            .addClass('dialog-toast');
         var container = dialog.getContainer();
         var height = dialog.height(container);
         dialog.getContainer().style.textAlign = 'center';
@@ -15,11 +16,10 @@ jDialog.extend({
             .hideHeader()
             .hideModal()
             .height(height)
-            .addClass('dialog-toast')
             .autoHide(delay || 3);
         return dialog;
     },
-    error: function(message, callBack) {
+    error: function (message, callBack) {
         return jDialog(message, callBack).addClass('dialog-error');
     }
 });
